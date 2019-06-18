64c439083a2be06f511c7a2749209a2b9fa0cc06
JungChoon Park <unlimitedfocus@gmail.com>
2019-06-19 01:53:50 +0900

    keep turbolinks

--- app/javascript/packs/pages/application/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue.js';
-import UsersNewView from '../../views/users/new';
-
-new Vue({
-  el: '#users-new-view',
-  components: {
-      'users-new-view': UsersNewView
-  }
-});
